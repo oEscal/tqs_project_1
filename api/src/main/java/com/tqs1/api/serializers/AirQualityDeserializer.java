0ef6172733531f2922bd6c3c0e637fa67434afd4
escaleira <pedroescaleira@hotmail.com>
2020-04-04 03:47:49 +0100

    error with serializer uid fixed

--- api/src/main/java/com/tqs1/api/serializers/AirQualityDeserializer.java
+++ api/src/main/java/com/tqs1/api/serializers/AirQualityDeserializer.java
@@ -1,4 +1,6 @@
 package com.tqs1.api.serializers;
 
 public class AirQualityDeserializer extends SimpleAirQualityDeserializer {
+
+    private static final long serialVersionUID = 6106269076155338045L;
 }
